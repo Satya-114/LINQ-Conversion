bdf2ffbb82ee9e336feb9a2b2226bd97f8a66292
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-17 17:21:53 +0530

    Add files via upload

---
+++ cast/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+namespace LINQ_Cast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList
+            {
+                10,
+                20,
+                30
+            };
+            IEnumerable<int> result = list.Cast<int>();
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
