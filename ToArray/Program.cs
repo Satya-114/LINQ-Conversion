d6bf5e25fcc13a0a3372cf3762bb63a433a9885f
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-17 17:07:45 +0530

    Add files via upload

---
+++ ToArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a List
+            List<int> numbersList = new List<int>()
+            {
+                10, 22, 30, 40, 50, 60
+            };
+            //Converting List to Array
+            int[] numbersArray = numbersList.ToArray();
+            foreach (var num in numbersArray)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
