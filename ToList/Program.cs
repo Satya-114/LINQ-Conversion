1f8dd10443cab037c12d962c22705ba6333c0d9f
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-17 17:03:46 +0530

    Add files via upload

---
+++ ToList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_ToList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating Integer Array
+            int[] numbersArray = { 10, 22, 30, 40, 50, 60 };
+            //Converting Integer Array to List using ToList method
+            List<int> numbersList = numbersArray.ToList();
+            foreach (var num in numbersList)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
